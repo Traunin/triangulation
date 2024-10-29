685197448cbbb4526836c1ddfe5f195565101b01
Traunin <traunin5@gmail.com>
2024-10-29 17:28:04 +0300

    feat: ghost vertex on edge hover

--- demo/src/main/java/com/github/traunin/triangulation_demo/PolygonDrawerCanvas.java
+++ demo/src/main/java/com/github/traunin/triangulation_demo/PolygonDrawerCanvas.java
@@ -16,6 +16,7 @@ import javafx.scene.shape.ArcType;
 public class PolygonDrawerCanvas {
     private final static Color DEFAULT_VERTEX_COLOR = Color.RED;
     private final static Color HIGHTLIGHT_VERTEX_COLOR = Color.YELLOW;
+    private final static Color GHOST_VERTEX_COLOR = new Color(1, 0, 0, 0.1);
     private final static float DEFAULT_POLYGON_SIZE = 100f;
     private final static int SIDE_COUNT = 6;
     private final static float VERTEX_SIZE = 20f;
@@ -68,6 +69,10 @@ public class PolygonDrawerCanvas {
         canvas.setOnMouseMoved(e -> {
             highlightVertex(e.getX(), e.getY());
             redraw();
+
+            if (selectedVertex == null) {
+                drawGhostVertex(e.getX(), e.getY(), canvas.getGraphicsContext2D());
+            }
         });
 
         canvas.setOnMouseDragged(e -> {
@@ -127,7 +132,7 @@ public class PolygonDrawerCanvas {
     }
 
     private void drawVertex(Vertex vertex, GraphicsContext ctx) {
-        ctx.setFill(vertex == selectedVertex ?  HIGHTLIGHT_VERTEX_COLOR : DEFAULT_VERTEX_COLOR);
+        ctx.setFill(vertex.getColor());
         ctx.fillArc(
             vertex.getX() - VERTEX_SIZE / 2,
             vertex.getY() - VERTEX_SIZE / 2,
@@ -139,23 +144,81 @@ public class PolygonDrawerCanvas {
         );
     }
 
+    private void drawGhostVertex(double mouseX, double mouseY, GraphicsContext ctx) {
+        for (Vertex vertex : vertices) {
+            if (isCursorOnEdge(
+                vertex.getX(), vertex.getY(), vertex.getConnected().getX(), vertex.getConnected().getY(), mouseX, mouseY
+            )) {
+                canvas.setCursor(Cursor.CROSSHAIR);
+                drawVertex(new Vertex(getPointOnEdge(vertex, mouseX, mouseY), GHOST_VERTEX_COLOR), ctx);
+            }
+        }
+    }
+
+    private boolean isCursorOnEdge(
+        double edgePoint1X, double edgePoint1Y, double edgePoint2X, double edgePoint2Y, double mouseX, double mouseY
+    ) {
+        double k = (float) (edgePoint2X - edgePoint1X) / (edgePoint2Y - edgePoint1Y);
+        double pointYOnLine1 = k * (edgePoint1X - mouseX) + edgePoint1Y;
+        double pointYOnLine2 = k * (edgePoint2X - mouseX) + edgePoint2Y;
+
+        boolean cursorIsInVerticesRect = mouseY >= pointYOnLine1 != mouseY >= pointYOnLine2;
+        if (!cursorIsInVerticesRect) {
+            return false;
+        }
+
+        double mouseToEdgeDistance = getDistanceFromPointToLine(
+            mouseX, mouseY, edgePoint1X, edgePoint1Y, edgePoint2X, edgePoint2Y
+        );
+
+        return  mouseToEdgeDistance <= VERTEX_SIZE / 2;
+    }
+
+    private double getDistanceFromPointToLine (double x0, double y0, double x1, double y1, double x2, double y2) {
+        return Math.abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) /
+                Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+    }
+
+    private Vector2f getPointOnEdge(Vertex vertex, double mouseX, double mouseY) {
+        Vector2f edgePoint1 = vertex.getPosition();
+        Vector2f edgePoint2 = vertex.getConnected().getPosition();
+
+        double edgeXLen = edgePoint2.getX() - edgePoint1.getX();
+        double edgeYLen = edgePoint2.getY() - edgePoint1.getY();
+
+        double edgeLenSquare = edgeXLen * edgeXLen + edgeYLen * edgeYLen;
+        double t = ((mouseX - edgePoint1.getX()) * (edgeXLen) + (mouseY - edgePoint1.getY()) * edgeYLen) / edgeLenSquare;
+
+        return new Vector2f(
+            (float) (edgePoint1.getX() + edgeXLen * t),
+            (float) (edgePoint1.getY() + edgeYLen * t)
+        );
+    }
+
     private void drawEdge(Vertex vertex, GraphicsContext ctx) {
-        ctx.strokeLine(vertex.getX(), vertex.getY(), vertex.getConnected().getX(), vertex.getConnected().getY());
+        if (vertex.getConnected() != null) {
+            ctx.strokeLine(vertex.getX(), vertex.getY(), vertex.getConnected().getX(), vertex.getConnected().getY());
+        }
     }
 
     private void highlightVertex(double x, double y) {
         for (Vertex vertex : vertices) {
             if (vertex.getDistance(x, y) <= VERTEX_SIZE / 2) {
                 selectedVertex = vertex;
+                vertex.setColor(HIGHTLIGHT_VERTEX_COLOR);
                 canvas.setCursor(Cursor.OPEN_HAND);
                 return;
             }
+        }
+        if (selectedVertex != null) {
+            selectedVertex.setColor(DEFAULT_VERTEX_COLOR);
         }
         canvas.setCursor(Cursor.DEFAULT);
         selectedVertex = null;
     }
 
     private static class Vertex {
+        private Color color;
         private Vector2f position;
         private Vertex connected;
 
@@ -164,8 +227,14 @@ public class PolygonDrawerCanvas {
             this.connected = connected;
         }
 
+        public Vertex(Vector2f position, Color color) {
+            this(position);
+            this.color = color;
+        }
+
         public Vertex(Vector2f position) {
             this.position = position;
+            this.color = DEFAULT_VERTEX_COLOR;
         }
 
         public Vector2f getPosition() {
@@ -190,6 +259,14 @@ public class PolygonDrawerCanvas {
 
         public float getY() {
             return position.getY();
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
         }
 
         public double getDistance(double x, double y) {
