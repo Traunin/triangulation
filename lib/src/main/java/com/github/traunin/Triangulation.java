c31dd02c93c337d313d4fef8404e5f38c0ac9daf
Traunin <traunin5@gmail.com>
2024-10-30 02:01:32 +0300

    feat: various signatures

--- lib/src/main/java/com/github/traunin/Triangulation.java
+++ lib/src/main/java/com/github/traunin/Triangulation.java
@@ -1,7 +1,43 @@
 package com.github.traunin;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Triangulation {
-    public static boolean testTest() {
-        return true;
+    public static <T extends Vector2f> List<T[]> convexPolygonTriangulate(List<T> vertices, List<Integer> vertexIndices) {
+        int vertexCount = vertexIndices.size();
+
+        if (vertexCount < 3) {
+            throw new TriangulationException("Not enough vertex indices for a polygon");
+        }
+
+        List<T> orderedVertices = new ArrayList<>(vertexCount);
+        for (int i = 0; i < vertexCount; i++) {
+            orderedVertices.set(i, vertices.get(vertexIndices.get(i)));
+        }
+
+
+        return convexPolygonTriangulate(orderedVertices);
+    }
+
+    @SafeVarargs
+    public static <T extends Vector2f> List<T[]> convexPolygonTriangulate(T... vertices) {
+        List<T> verticesList = Arrays.stream(vertices).toList();
+
+        return convexPolygonTriangulate(verticesList);
+    }
+
+    public static <T extends Vector2f> List<T[]> convexPolygonTriangulate(List<T> vertices) {
+        int vertexCount = vertices.size();
+
+        if (vertexCount < 3) {
+            throw new TriangulationException("Not enough vertex indices for a polygon");
+        }
+
+        List<T[]> triangles = new ArrayList<>(vertexCount - 2);
+
+        return triangles;
     }
 }
