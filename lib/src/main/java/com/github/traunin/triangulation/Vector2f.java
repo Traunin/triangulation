36388a0c5094a9e5608c5d8f53db4a2d425c5844
Traunin <traunin5@gmail.com>
2024-11-01 22:52:16 +0300

    docs: add javadoc

--- lib/src/main/java/com/github/traunin/triangulation/Vector2f.java
+++ lib/src/main/java/com/github/traunin/triangulation/Vector2f.java
@@ -1,6 +1,19 @@
 package com.github.traunin.triangulation;
 
+/**
+ * Represents a point in 2D space.
+ * <p> Holds x and y coordinates as floats.
+ */
 public interface Vector2f {
+    /**
+     * Returns the x coordinate of the point.
+     * @return x coordinate
+     */
     float x();
+
+    /**
+     * Returns the y coordinate of the point.
+     * @return y coordinate
+     */
     float y();
 }
