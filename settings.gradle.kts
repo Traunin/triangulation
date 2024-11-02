447839b8047b133b0ee3113f269d71a3d1f03952
Traunin <traunin5@gmail.com>
2024-11-02 19:00:44 +0300

    build: apply foojay-resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
 rootProject.name = "triangulation"
 include("lib")
 include("demo")
+
+plugins {
+    // automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
